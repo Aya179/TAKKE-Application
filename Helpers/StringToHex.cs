01b9c8724eaa2b07a5dbf7e5040d713e91e39614
Aya179 <81487304+Aya179@users.noreply.github.com>
2022-03-12 22:55:34 +0200

    Add files via upload

---
+++ Helpers/StringToHex.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Takke.Helpers
+{
+    public class StringToHex
+    {
+        public static string ConvertStringToHex(string str)
+        {
+            var result = "";
+            for (var i = 0; i < str.Length; i++)
+            {
+                string o = Convert.ToString(str[i], 16);
+
+                if (o.Length == 1)
+                {
+                    result += "000" + o;
+                }
+                else if (o.Length == 2)
+                {
+                    result += "00" + o;
+                }
+                else if (o.Length == 3)
+                {
+                    result += "0" + o;
+                }
+                else
+                {
+                    result += o;
+                }
+            }
+            return result;
+        }
+    }
+}
